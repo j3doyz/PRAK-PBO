52091937205b05e5919164b3b734dcee88c15512
Muhammad Zedi Mumtaz <145269825+j3doyz@users.noreply.github.com>
2024-03-02 20:39:02 +0700

    Create 122140223_Muhammad Zedi Mumtaz_Soal 1.py
    Tugas Praktikum PBO RB Minggu 1 Soal 1

---
+++ prak1/122140223_Muhammad Zedi Mumtaz_Soal 1.py
@@ -0,0 +1,16 @@
+#Muhammad Zedi Mumtaz
+#122140223
+#Tugas Praktikum PBO RB MG 1 Soal 1
+
+bawah= int(input("Inputkan Batas Bawah Yang Anda Inginkan : "))
+atas = int(input("Inputkan Batas Atas Yang Anda Ingnkan : "))
+
+if bawah < 0 or atas < 0 :
+    print("Maaf, batas bawah dan atas yang dimasukan tidak boleh di bawah nol")
+else :
+    sum = 0
+    for x in range (bawah, atas) :
+        if x % 2 == 1 :
+            print(x)
+            sum += x
+    print("Total : ", sum)
